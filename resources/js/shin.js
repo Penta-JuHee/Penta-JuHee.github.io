6c585167f62e1317b2194d50bfc6950edbdf9b2b
Penta-JuHee <125944048+Penta-JuHee@users.noreply.github.com>
2023-04-28 15:08:23 +0900

    Update shin.js

--- resources/js/shin.js
+++ resources/js/shin.js
@@ -36,3 +36,13 @@ function slide() {
 }
 
 setInterval(slide, 2000);
+
+/** navbar */
+const toggleBtn = document.querySelector(".navbar__toggleBtn");
+const menu = document.querySelector(".navbar__menu");
+const icons = document.querySelector(".navbar__icons");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  icons.classList.toggle("active");
+});
